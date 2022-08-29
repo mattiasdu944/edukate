82e0fea2a25ec91b9622b972c38b21d930e9b430
mattiasdu944 <duartemattias3@gmail.com>
2022-08-29 11:43:00 -0400

    se agrego el componente navbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from './Navbar'
 
 const Layout = () => {
   return (
     <>
-        <div>Navbar cons testing</div>
+        <Navbar/>
         <Outlet/> 
         <div className="">
             Footer
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+        <nav>
+            <h1>Logo</h1>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
