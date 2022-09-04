8f926e5bcc71eac1b3a3eb0e92f99e60c9f350b0
Mattias <duartemattias3@gmail.com>
2022-09-03 20:29:08 -0400

    se agregaron las env

--- src/backend/supabase.js
+++ src/backend/supabase.js
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
-  "https://qjzaxgwtmenwwhddpvuh.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqemF4Z3d0bWVud3doZGRwdnVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjIwNzcyNzEsImV4cCI6MTk3NzY1MzI3MX0.vX_3Ylhrz_-PcHguD_imCHEOlOmV6U6AQBXRPQcmBnI"
+  import.meta.env.VITE_APP_SUPABASE_KEY,
+  import.meta.env.VITE_APP_SUPABASE_ANON_KEY
 );
