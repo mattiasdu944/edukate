22beb012e794b3d844e3da91518a03712e468c88
Ultron <duartemattias4@gmail.com>
2022-09-06 17:39:32 -0400

    se agrego hasrouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter, HashRouter} from "react-router-dom";
 import {theme} from './constants/theme'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   
   <React.StrictMode>
     <ChakraProvider theme={theme}>
+      <HashRouter>
 
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+          <App />
+        
+      </HashRouter>
 
     </ChakraProvider>
   </React.StrictMode>
